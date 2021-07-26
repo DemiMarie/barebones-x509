cd7338b815e1346b2a0404e1c0070afaba9c78ef
Demi Marie Obenour <demiobenour@gmail.com>
2021-07-26 03:46:14 -0400

    Replace x509-signature with barebones-x509
    The old crate name is wrong.

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,40 @@
 //! # A low-level X.509 parsing and certificate signature verification library.
 //!
-//! x509-signature can verify the signatures of X.509 certificates, as well as
+//! barebones-x509 can verify the signatures of X.509 certificates, as well as
 //! certificates made by their private keys.  It can also verify that a
 //! certificate is valid for the given time. However, it is (by design) very
 //! low-level: it does not know about *any* X.509 extensions, and does not parse
 //! distinguished names at all.  It also provides no path-building facilities.
 //! As such, it is not intended for use with the web PKI; use webpki for that.
 //!
-//! x509-signature’s flexibiity is a double-edged sword: it allows it to be used
+//! barebones-x509’s flexibiity is a double-edged sword: it allows it to be used
 //! in situations where webpki cannot be used, but it also makes it
-//! significantly more dangerous.  As a general rule, x509-signature will accept
+//! significantly more dangerous.  As a general rule, barebones-x509 will accept
 //! any certificate that webpki will, but it will also accept certificates that
-//! webpki will reject.  If you find a certificate that x509-signature rejects
+//! webpki will reject.  If you find a certificate that barebones-x509 rejects
 //! and webpki rejects, please report it as a bug.
 //!
-//! x509-signature was developed for use with
+//! barebones-x509 was developed for use with
 //! [libp2p](https://github.com/libp2p), which uses certificates that webpki
 //! cannot handle.  Its bare-bones design ensures that it can handle almost any
 //! conforming X.509 certificate, but it also means that the application is
 //! responsible for ensuring that the certificate has valid X.509 extensions.
-//! x509-signature cannot distinguish between a certificate valid for
-//! `mozilla.org` and one for `evilmalware.com`!  However, x509-signature
+//! barebones-x509 cannot distinguish between a certificate valid for
+//! `mozilla.org` and one for `evilmalware.com`!  However, barebones-x509
 //! does provide the hooks needed for higher-level libraries to be built on top
 //! of it.
 //!
-//! Like webpki, x509-signature is zero-copy and `#![no_std]` friendly.  If
-//! built without the `alloc` feature, x509-signature will not rely on features
+//! Like webpki, barebones-x509 is zero-copy and `#![no_std]` friendly.  If
+//! built without the `alloc` feature, barebones-x509 will not rely on features
 //! of *ring* that require heap allocation, specifically RSA.
 //!
-//! x509-signature should never panic on any input, regardless of its
+//! barebones-x509 should never panic on any input, regardless of its
 //! configuration options.  If it does panic, it is considered a security
 //! vulnerability and will be fixed with the highest priority.
 //!
 //! ## Features
 //!
-//! `x509-signature` is highly configurable by means of compile-time options.
+//! `barebones-x509` is highly configurable by means of compile-time options.
 //! Code that is not used by most users is off by default and must be enabled by
 //! means of a cargo feature.  This reduces the attack surface of normal builds.
 //!
@@ -46,7 +46,7 @@
 //!   `subjectUniqueId` and `issuerUniqueId` fields.  This is off by default.
 //!   The `subjectUniqueId` and `issuerUniqueId` fields available as the
 //!   `unique_id` field on the [`X509Certificate`] struct.  This feature is made
-//!   available so that `x509-signature` can claim to be able to parse any valid
+//!   available so that `barebones-x509` can claim to be able to parse any valid
 //!   X.509 certificate.  If you do need to enable it, please e-mail me at
 //!   <demiobenour@gmail.com> explaining the reason.
 
